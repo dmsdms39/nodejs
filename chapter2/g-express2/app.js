fd65f02850f32f2aecf8299859bdea96313fd4f5
Cho Eun Hye <series39@naver.com>
2019-12-27 17:36:02 +0900

    app.js 기본세팅"

--- chapter2/g-express2/app.js
+++ chapter2/g-express2/app.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var app = express()
 var path = require('path')
-require('dotenv').comfig()
+require('dotenv').config()
 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 app.set('html', require('ejs').renderFile)
-
+app.use(express.json())
+app.use(express.static(path.join(__dirname,'public')))
 
 var port = process.env.PORT || 3000
 app.listen(port, ()=>{
