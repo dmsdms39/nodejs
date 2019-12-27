2d97265a577ddf0ff4fecc89b7ebbc6d48848427
Cho Eun Hye <series39@naver.com>
2019-12-27 18:24:56 +0900

    first commit

--- chapter2/g-express2/app.js
+++ chapter2/g-express2/app.js
@@ -3,13 +3,19 @@ var app = express()
 var path = require('path')
 require('dotenv').config()
 
+var indexRouter = require('./router/index.js')
+var userRouter = require('./router/user.js')
+
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 app.set('html', require('ejs').renderFile)
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use('/', indexRouter);
+app.use('/people', userRouter);
+
 var port = process.env.PORT || 3000
 app.listen(port, ()=>{
-    console.log(`Server is Starting : : http://localhost:${port}`)
+    console.log(`Server is Starting :http://localhot:${port}` )
 })
